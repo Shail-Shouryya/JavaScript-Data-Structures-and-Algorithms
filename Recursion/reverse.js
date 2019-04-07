6fa125c074aceae85b8f330c6cdf1758ae8523c8
Shail Shouryya <sshouryy@ucsc.edu>
2019-04-06 20:29:13 -0700

    added solutions to recursion problems

--- Recursion/reverse.js
+++ Recursion/reverse.js
@@ -1,5 +1,9 @@
-function reverse() {
+function reverse(str) {
   // add whatever parameters you deem necessary - good luck!
+  reverseArr = [];
+  if (str) {
+    return reverseArr.push(str.slice(-1).concat(reverse(str.slice(0, -1))));
+  }
 }
 
 // reverse('awesome') // 'emosewa'
