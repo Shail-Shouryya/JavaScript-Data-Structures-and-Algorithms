7d54402e4ffa0296e3caecd4e8d2435dc51149fa
Shail-Shouryya <shailshouryya@gmail.com>
2019-06-13 23:42:04 -0700

    added get method for singly ll

--- DataStructures/practice.js
+++ DataStructures/practice.js
@@ -65,4 +65,13 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    get(index){
+        if (index < 0 || index >= this.length) return undefined;
+        var counter = 0;
+        var current = this.head;
+        while (counter !== index){
+            current = current.next;
+            counter++;
+        } return current;
+    }
 }
